d0aefea9ef16ae04e11328e250700c867fd27061
nasim-saaidi <115530133+nasim-saaidi@users.noreply.github.com>
2026-09-16 18:03:00 +0200

    javascript in commentaar gezet

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,22 @@
-console.log("js connected")
+// console.log("js connected")
 
 
 
-class calculator {
-    add(a, b) {
-        return a + b;
-    }
-}
+// class calculator {
+//     add(a, b) {
+//         return a + b;
+//     }
+// }
 
-const minus = (a, b) => a - b
-
-
-function divide(a, b) {
-    return a / b;
-}
+// const minus = (a, b) => a - b
 
 
-const calc = new calculator();
-console.log(calc.add(1, 2))
-console.log(minus(4, 2))
-console.log(divide(10, 2))
+// function divide(a, b) {
+//     return a / b;
+// }
+
+
+// const calc = new calculator();
+// console.log(calc.add(1, 2))
+// console.log(minus(4, 2))
+// console.log(divide(10, 2))
